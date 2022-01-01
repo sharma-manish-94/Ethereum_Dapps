e537ff168592d63741f3cf0a57b8d272fd3e6a39
code-manish <code.manish94@gmail.com>
2022-01-01 17:43:22 +0530

    Hello World program

---
+++ HelloWorld/contracts/HelloWorld.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    string private helloMessage = "Hello world";
+
+    function getHelloMessage() public view returns (string memory) {
+        return helloMessage;
+    }
+}
