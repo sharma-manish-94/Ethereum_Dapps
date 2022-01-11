c0934977be9252a2f97eccb853cd1d07106a621a
code-manish <code.manish94@gmail.com>
2022-01-11 15:43:32 +0530

    use of struct

--- IntroToSolidity/contracts/SimpleStorage.sol
+++ IntroToSolidity/contracts/SimpleStorage.sol
@@ -1,15 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract SimpleStorage{
-    uint storedData;
+contract SimpleStorage {
+    uint256 storedData;
 
-    function set(uint x) public{
+    function set(uint256 x) public {
         storedData = x;
     }
 
-    function get() public view returns(uint){
+    function get() public view returns (uint256) {
         return storedData;
     }
 
+    struct Person {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    Person[] public people;
+    mapping(string => uint256) namToFavouriteNumber;
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(Person(_favouriteNumber, _name));
+        namToFavouriteNumber[_name] = _favouriteNumber;
+    }
 }
